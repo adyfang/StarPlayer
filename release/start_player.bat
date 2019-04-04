19d7d3414a1a06255aef43e020621a2d2ba347d4
adyfang <adyfang@outlook.com>
2019-04-04 19:57:59 +0800

    1、更新启动脚本，增加32/64位和JAVA环境变量判断； 2、github自动生成的.gitignore将jar等文件过滤了，导致部分文件未上传；

--- release/start_player.bat
+++ release/start_player.bat
@@ -1,1 +1,21 @@
-start ./jre7/bin/javaw -Dfile.encoding=utf-8 -jar StarPlayer.jar
+@echo off
+rem
+rem     https://github.com/adyfang/StarPlayer.git
+rem
+if "%PROCESSOR_ARCHITECTURE%"=="x86" goto x86
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto x64
+if "%JAVA_HOME%" == "" goto noJavaHome
+
+:x86
+echo Please install JDK1.7 at first.
+goto noJavaHome
+:x64
+start /min ./jre7/bin/javaw -Dfile.encoding=utf-8 -jar StarPlayer.jar
+goto exit
+
+:noJavaHome
+echo The JAVA_HOME environment variable is not defined correctly.
+echo NOTE: JAVA_HOME should point to a JDK not a JRE.
+pause
+goto exit
+
